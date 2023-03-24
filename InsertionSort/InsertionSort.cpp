b8201270902346dcde4b67aea7a0ec3b9a9064e9
ulfhiraa <studyiraa@gmail.com>
2023-03-24 15:42:24 +0700

    Membuat tampilan susunan data element array

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -17,8 +17,8 @@ void input() {		// Procedure Input
 			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; // Menampilkan pesan jika data lebih dari 20
 		}
 	} 
-	cout << endl;								// Membuat jarak perbaris program
-	cout << "======================" << endl;
+	cout << endl;								// Membuat jarak per baris program
+	cout << "======================" << endl;   // Membuat tampilan susunan data element array
 	cout << "Masukkan Element Array" << endl;
 	cout << "======================" << endl;
 
