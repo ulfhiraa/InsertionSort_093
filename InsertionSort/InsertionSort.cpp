a0cb6c8342ac109cfa587b8aca3838c5a729b628
ulfhiraa <studyiraa@gmail.com>
2023-03-24 15:13:25 +0700

    Procedure Input

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -3,3 +3,70 @@ using namespace std;
 
 int arr[20];			// membuat array dengan panjang data 20
 int n;					// membuat variable inputan n
+
+void input() {
+	while (true) {
+		cout << "Masukkan jumlah data pada array : ";
+		cin >> n;
+
+		if (n <= 20) {
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "======================" << endl;
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
+}
+
+void insertionsort() {
+	int temp;
+	int j;
+
+	for (int i = 1; i < n; i++) {
+
+		temp = arr[i];
+
+		j = i - 1;
+
+		while (j >= 0 && arr[j] > temp)
+
+		{
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = temp;
+
+		cout << "\nPass " << i << ": ";
+		for (int k = 0; k < n; k++) {
+			cout << arr[k] << " ";
+		}
+	}
+}
+
+void display() {
+	cout << "\n===============================" << endl;
+	cout << "Element Array Yang Telah Tersusun" << endl;
+	cout << "=================================" << endl;
+
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << " ";
+	}
+}
+
+int main()
+{
+	input();
+	insertionsort();
+	display();
+}
